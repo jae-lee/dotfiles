d169f9e0c007e37d11ae72553faf95d4b6692474
Jae Lee <jae@dustral.com>
2015-06-01 03:17:46 +0000

    Modify to reflect updates to oh-my-fish

--- config.fish
+++ config.fish
@@ -1,19 +1,20 @@
+# Remove fish default greeting
+set --erase fish_greeting
+
 # Path to your oh-my-fish.
 set fish_path $HOME/.oh-my-fish
-
-# Theme
-set fish_theme gnuykeaj
-
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-set fish_plugins tmux rbenv pbcopy
 
 # Path to your custom folder (default path is $FISH/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
 
 # Load oh-my-fish configuration.
 . $fish_path/oh-my-fish.fish
+
+Theme "gnuykeaj"
+
+Plugin "pbcopy"
+Plugin "rbenv"
+Plugin "tmux"
 
 if status --is-login
   set PATH $PATH /usr/local/bin /usr/bin /bin $HOME/bin $HOME/.rbenv/bin
